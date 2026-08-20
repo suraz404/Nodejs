0c066c79185b2d5a39a9289c7fa6fa4aab104625
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-08-20 17:09:41 +0545

    feat: implement user creation endpoint and export User model

--- mongoDbLearn/index.js
+++ mongoDbLearn/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { User } from "./models/user.model.js";
 const PORT = 8100;
 
 const app = express();
+app.use(express.json());
 const url = process.env.URL;
 
 const connectDb = async () => {
@@ -17,6 +19,19 @@ const connectDb = async () => {
 
 app.get("/", (req, res) => {
   res.send({ sucess: true });
+});
+app.post("/create", async (req, res) => {
+  try {
+    let { name, age, email } = req.body;
+    const newUser = await User.create({
+      name,
+      age,
+      email,
+    });
+    res.status(201).json({ message: "User Created" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 app.listen(PORT, () => {
--- mongoDbLearn/models/user.model.js
+++ mongoDbLearn/models/user.model.js
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
