abf7eff64a4c8d60d77fa2c85f4c4a139115bc0e
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-08-30 22:42:09 +0545

    feat: add authentication protection to logout route

--- Authentication/backendd/routes/auth.routes.js
+++ Authentication/backendd/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.js";
 import { upload } from "../middleware/multer.js";
+import { protect } from "../middleware/checkAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", upload.single("profilePicture"), signup);
 authRouter.post("/login", login);
-authRouter.post("/logout", logout);
+authRouter.post("/logout", protect, logout);
 
 export default authRouter;
